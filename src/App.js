3356732f3d1e5849f213ae1ec8308a72a36e48ba
DanielC <dacm1311@hotmail.com>
2022-08-01 20:21:16 -0500

    adding buttons to handle state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import AddingNumber from './components/addingNumber';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [emotion, setEmotion] = useState("Happy");
+
+  useEffect(() => {
+    console.log(emotion)
+  },[emotion])
+
   return (
     <div className='App'>
-      <h1>Hello from React App</h1>
+      <h1>This is my emotion {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>excited</button>
     </div>
   );
 }
