06d157fe6be8900aef3efdac2a4fad5b63617345
DanielC <dacm1311@hotmail.com>
2022-08-02 19:39:31 -0500

    adding first fetch for async react

---
+++ src/AppDataLoad.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(()=>{
+        fetch('https://api.github.com/users/dcanonmolina')
+        .then((response) => response.json())
+        .then(setData);
+    },[]);
+
+    if(data){
+        return (<pre>{JSON.stringify(data)}</pre>)
+    }
+
+  return (
+    <h1>Data</h1>
+  );
+}
+
+export default App;
